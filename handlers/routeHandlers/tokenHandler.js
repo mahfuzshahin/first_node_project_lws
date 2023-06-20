0c943ae217e169374e404c6da71043fb4190cef5
mahfuzswe <mahfuzur.web@daffodilvarsity.edu.bd>
2023-06-20 16:05:33 +0600

    Updated

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -1,6 +1,7 @@
 const data = require("../../lib/data");
-//const { hash } = require("../../helpers/utilities");
-//const { parseJSON } = require("../../helpers/utilities");
+const { hash } = require("../../helpers/utilities");
+const { parseJSON } = require("../../helpers/utilities");
+const { createRandomString } = require("../../helpers/utilities");
 const handler = {};
 
 handler.tokenHandler = (requestProperties, callback) => {
@@ -13,7 +14,43 @@ handler.tokenHandler = (requestProperties, callback) => {
   }
 };
 handler._token = {};
-handler._token.post = (requestProperties, callback) => {};
+handler._token.post = (requestProperties, callback) => {
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
+  if (phone && password) {
+    data.read("users", phone, (err1, userData) => {
+      let hashedPassword = hash(password);
+      if (hashedPassword === parseJSON(userData).password) {
+        let tokenId = createRandomString(20);
+        let expires = Date.now() + 60 * 60 * 1000;
+        let tokenObject = {
+          phone,
+          id: tokenId,
+          expires,
+        };
+        data.create("tokens", tokenId, tokenObject, (err2) => {
+          if (!err2) {
+            callback(200, tokenObject);
+          } else {
+            callback(500, { error: "there was a problem" });
+          }
+        });
+      } else {
+        callback(400, { error: "Password is not valid" });
+      }
+    });
+  } else {
+    callback(400, { error: "You have a problem" });
+  }
+};
 handler._token.get = (requestProperties, callback) => {};
 handler._token.put = (requestProperties, callback) => {};
 handler._token.delete = (requestProperties, callback) => {};
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -23,5 +23,24 @@ utilities.hash = (str) => {
   return false;
 };
 
+//create random string
+utilities.createRandomString = (strlength) => {
+  let length = strlength;
+  length = typeof strlength === "number" && strlength > 0 ? strlength : false;
+  if (length) {
+    const possibleCharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let output = "";
+    for (let i = 1; i < length; i += 1) {
+      let randomCharacter = possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  } else {
+    return false;
+  }
+};
+
 // export module
 module.exports = utilities;
