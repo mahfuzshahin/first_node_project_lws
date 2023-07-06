b4f7f480c5c14e0b9c698c87fdd9ce873a6ab44f
mahfuzswe <mahfuzur.web@daffodilvarsity.edu.bd>
2023-07-06 13:55:03 +0600

    Updated

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
-const http = require("http");
+const server = require("./lib/server");
+const workers = require("./lib/worker");
 
-const { handleReqRes } = require("./helpers/handleReqRes");
-const environemnt = require("./helpers/environments");
-const data = require("./lib/data");
-const { sendTwilioSms } = require("./helpers/notifications");
 const app = {};
-sendTwilioSms("01724808055", "Hello World", (err) => {
-  console.log(`this is the error`, err);
-});
-// data.create(,
-//   "test",
-//   "newFile",
-//   { name: "Bangladesh", language: "Bangla" },
-//   function (err) {
-//     console.log(`error was`, err);
-//   }
-// );
-// data.read("test", "newFile", (err, data) => {
-//   console.log(err, data);
-// });
-// data.update(
-//   "test",
-//   "newFile",
-//   { name: "Eangladesh", language: "Eangla" },
-//   (err) => {
-//     console.log(err);
-//   }
-// );
-// data.delete("test", "newFile", (err) => {
-//   console.log(err);
-// });
-//update existing file
-//create server
 
-app.createServer = () => {
-  const server = http.createServer(app.handelReqRes);
-  server.listen(environemnt.port, () => {
-    //console.log(`environment variable is ${process.env.NODE_ENV}`);
-    console.log(`listen port ${environemnt.port} `);
-  });
+app.init = () => {
+  // start the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-//handle request response
+app.init();
 
-app.handelReqRes = handleReqRes;
-app.createServer();
+module.exports = app;
